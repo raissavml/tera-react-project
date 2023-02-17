a4a20b6cee4f72668beb8a6c98aef022809b53aa
Raíssa <raissavml@hotmail.com>
2023-02-16 21:57:52 -0300

    este git contém roteamento

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import Home from ".components/pages/Home"
-import Home from ".components/pages/UserBlog"
-import Home from ".components/pages/Users"
-import ".styles/normalize.css"
-import "./styles/fontwesome.min.css"
-import "./styles/main.css"
+import { BrouserRouter, Routes, Route} from "react-router-dom";
+
+import Home from ".components/pages/Home";
+import Home from ".components/pages/UserBlog";
+import Home from ".components/pages/Users";
+
+import ".styles/normalize.css";
+import "./styles/fontwesome.min.css";
+import "./styles/main.css";
 
 function App() {
-  return  <Home />;
+  return (
+    <BrouserRouter>
+    <Routes>
+      < Route path="/" element={<Home />} />
+      < Route path="/users" element={<Users />} />
+      < Route path="/:userId" element={<UserBlog />} />
+      < Route path="/:userId/posts/new" element={<UserPostForm />} />
+      < Route path="*" element={<div>Not found</div>} />
+    </Routes>
+    </BrouserRouter>
+  )
 }
 
 export default App;
