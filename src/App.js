160d64a2ea292d964e3a005c2dce77fd0187d6a5
Raíssa <raissavml@hotmail.com>
2023-02-16 21:29:51 -0300

    Migrando HTML Users e UserBlog

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import Home from ".components/pages/Home"
+import Home from ".components/pages/UserBlog"
+import Home from ".components/pages/Users"
 import ".styles/normalize.css"
 import "./styles/fontwesome.min.css"
 import "./styles/main.css"
